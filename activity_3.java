684dae1d8dbfe8ca3f165325ba17d11684de2ad6
Moo <S3947523@student.rmit.edu.au>
2023-10-19 17:35:53 +1100

    Did activity 3
    stinky code :(

--- activity_3.java
+++ activity_3.java
@@ -7,19 +7,16 @@ import java.util.List;
  * Department in an organisation
  */
 public class Department {
-    
+
     private String name;
-    
+
     private ArrayList<String> ID1 = new ArrayList<String>();
-    
+
     private ArrayList<String> temp1 = new ArrayList<String>();
 
-    private ArrayList<int> employee = new ArrayList<int>();
+    private ArrayList<int[]> employee = new ArrayList<int[]>();
 
-    
     private int ID2;
-
-    private String name;
 
     public String getID() {
         return ID2;
@@ -29,34 +26,24 @@ public class Department {
         this.ID2 = ID;
     }
 
-    
-    public boolean checkEmployee(int eID) {
-
-	int p=0;
-	for (int i = 0; i < this.employee.size(); i++){
-check=this.employee.get(i);
-            if (check==eID) {
-                p=1;
+    public boolean checkEmployee(int employeeID) {
+        // Iterate through employees
+        for (int i = 0; i < this.employee.size(); i++) {
+            check = this.employee.get(i);
+            // Check if current employee is the one
+            if (check == employeeID) {
+                System.out.println("Employee exists");
+                return true;
             }
         }
-
-        if (p==1)
-{
-System.out.println("Employee exists");
-return true;
-}
-else{
-System.out.println("Employee doesn't exists");
-return false;
-	    }
+        // Employee was not found
+        System.out.println("Employee doesn't exist");
+        return false;
     }
-
-    
 
     public void assignNew(string temp2, int ID) {
         this.temp1.add(temp2);
-	  this.ID1.add(ID);
+        this.ID1.add(ID);
     }
-
 
 }
